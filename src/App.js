bf0c993abe90a34b8d81f57068518645fb999b93
rodas007 <sergioproba@gmail.com>
2022-04-05 19:56:04 +0200

    FIRST commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.scss';
+//import HomePage from './pages/HomePage/HomePage';
+import { Container, Row, Col } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container>
+    <Row>
+      <Col><img> </img></Col>
+      <Col>2 of 2</Col>
+    </Row>
+    <Row>
+      <Col>1 of 3</Col>
+      <Col>2 of 3</Col>
+      <Col>3 of 3</Col>
+    </Row>
+  </Container>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import "./HomePage.scss"
+
+import Alert from 'react-bootstrap/Alert';
+
+export default function HomePage() {
+    
+  return (
+
+
+    <>Esto es la HomePage
+    
+
+ <span classname="row">
+ 
+<div className="cuadrado"/>
+<div className="cuadrado1"/>
+<div className="cuadrado2"/>
+<div className="cuadrado3"/>
+<div className="cuadrado4"/>
+</span>
+
+<Alert dismissible variant="danger">
+      <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+      <p>
+        Change this and that and try again.
+      </p>
+    </Alert>
+  
+
+    
+
+
+</>
+  )
+}
